05a76eb3116ead0179b5015510482a73163a5b8a
korla <johannes.bengtsson@raysearchlabs.com>
2016-07-11 21:00:59 +0200

    Inline function to named

--- src/evaluateSpot/evaluateSpot.js
+++ src/evaluateSpot/evaluateSpot.js
@@ -3,7 +3,7 @@ var {noOverlap} = require('../noOverlap/noOverlap');
 var {section} = require('../section/section');
 var {evaluatePiece} = require('../evaluatePiece/evaluatePiece');
 
-function *coordinates(grid, [startX, startY], [endX, endY]) {
+function *coordinates(spot, [startX, startY], [endX, endY]) {
   for(var y = startY; y < endY; y++) {
     for(var x = startX; x < endX; x++) {
       yield [x,y];
@@ -11,28 +11,30 @@ function *coordinates(grid, [startX, startY], [endX, endY]) {
   }
 }
 
-var join = (grid, piece, [startX, startY]) => {
-  var joined = grid.map(r => r.map(c => c));
-  [...coordinates(grid, [0,0], [piece[0].length, piece.length])]
+var join = (spot, piece, [startX, startY]) => {
+  var joined = spot.map(r => r.map(c => c));
+  [...coordinates(spot, [0,0], [piece[0].length, piece.length])]
     .forEach(([x,y]) => joined[startY + y][startX + x] = piece[y][x]);
   return joined;
 }
 
-var evalPieces = grid =>
-  ![...coordinates(grid, [0,0], [grid[0].length - 2, grid.length - 2])]
-    .some(([x,y]) => !evaluatePiece(section(grid, [x,y], [x+2, y+2])));
+var sectionFromCoordinate = spot => ([x,y]) => section(spot, [x-1,y-1], [x+1, y+1]);
+var evalPieces = spot =>
+  [...coordinates(spot, [1,1], [spot[0].length-1, spot.length-1])]
+    .map(sectionFromCoordinate(spot))
+    .every(evaluatePiece);
 
-var expectGridAndPiece = (grid, piece) => {
-  expectGrid(grid);
+var expectGridAndPiece = (spot, piece) => {
+  expectGrid(spot);
   expectGrid(piece);
-  expect(grid.length === piece.length + 2);
-  expect(grid[0].length === piece[0].length + 2);
+  expect(spot.length === piece.length + 2);
+  expect(spot[0].length === piece[0].length + 2);
   return true;
 }
 
-var evaluateSpot = (grid, piece) =>
-  expectGridAndPiece(grid, piece) &&
-  noOverlap(section(grid, [1,1], [grid[0].length - 2, grid.length - 2]), piece) &&
-  evalPieces(join(grid, piece, [1,1]))
+var evaluateSpot = (spot, piece) =>
+  expectGridAndPiece(spot, piece) &&
+  noOverlap(section(spot, [1,1], [spot[0].length - 2, spot.length - 2]), piece) &&
+  evalPieces(join(spot, piece, [1,1]))
 
 export {evaluateSpot};
