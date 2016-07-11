a6c20db7c41b4eed9bac72f638c8fdec9c640187
korla <johannes.bengtsson@raysearchlabs.com>
2016-07-11 20:18:24 +0200

    Rewrite using data and every

--- src/evaluatePiece/evaluatePiece.js
+++ src/evaluatePiece/evaluatePiece.js
@@ -1,23 +1,23 @@
 var {expect, expectGrid} = require('../utils/expect');
 
 var directions = {
-  up: 0,
-  right: 1,
-  down: 2,
-  left: 3
+  U: 0,
+  R: 1,
+  D: 2,
+  L: 3
 }
 
-var matches = spot => (dir1, p2, dir2) => spot.empty || p2.empty || spot.roads[dir1] === p2.roads[dir2];
+var roadsMatch = spot => ([dir1, p2, dir2]) => spot.empty || p2.empty || spot.roads[dir1] === p2.roads[dir2];
 
 var evaluatePiece = evalgrid => {
   expectGrid(evalgrid);
   expect(evalgrid.length === 3, `Evalgrids are expected to be of size 3, but is instead ${evalgrid.length}`);
-  var spotMatches = matches(evalgrid[1][1]);
-  var topMatches = spotMatches(directions.up, evalgrid[0][1], directions.down);
-  var rightMatches = spotMatches(directions.right, evalgrid[1][2], directions.left);
-  var bottomMatches = spotMatches(directions.down, evalgrid[2][1], directions.up);
-  var leftMatches = spotMatches(directions.left, evalgrid[1][0], directions.right);
-  return topMatches && rightMatches && bottomMatches && leftMatches;
+  return [
+    [directions.U, evalgrid[0][1], directions.D],
+    [directions.R, evalgrid[1][2], directions.L],
+    [directions.D, evalgrid[2][1], directions.U],
+    [directions.L, evalgrid[1][0], directions.R]
+  ].every(roadsMatch(evalgrid[1][1]));
 }
 
 export {evaluatePiece};
